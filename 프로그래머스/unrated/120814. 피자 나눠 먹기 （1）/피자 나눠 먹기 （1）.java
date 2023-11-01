5cb4c2dd74ddc4be3ed343e3b01fdc1fc308e7ad
seungchan <149456209+seungchanni@users.noreply.github.com>
2023-11-01 09:23:05 +0900

    [level unrated] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        answer = n/7;
+        if(n%7>0){
+            answer++;
+        }
+        return answer;
+    }
+}
