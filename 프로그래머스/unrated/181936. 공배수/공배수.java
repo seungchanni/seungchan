49d5380c4bedd84960ebe27dddadd7d8f72c0ed9
seungchan <149456209+seungchanni@users.noreply.github.com>
2023-11-03 09:21:18 +0900

    [level unrated] Title: 공배수, Time: 0.01 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181936. 공배수/공배수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        
+        if(number%n==0){
+            if(number%m==0){
+                answer=1;
+            }
+            else{
+                answer=0;
+            }
+        }
+        else{
+            answer=0;
+        }
+        
+        return answer;
+    }
+}
